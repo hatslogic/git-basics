11e3d0f9ad30e89c427415efc5660f6f0b012d91
Shinu <shinu@2hatslogic.com>
2021-08-30 17:16:07 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+
+<head>
+  <title>The Git & Github Bootcamp!  | Master branch</title>
+</head>
+
+<body>
+  <h2>What is a Git Repo</h2>
+  <p>Git is software for tracking changes in any set of files, usually used for coordinating work among programmers collaboratively developing source code during software development.
+
+  </p>
+</body>
+
+</html>
