ded603910e23619d9034d2d256bde2498d9bc510
Shinu <shinu@2hatslogic.com>
2021-09-05 12:46:17 +0530

    Removed unwanted space.

--- index.php
+++ index.php
@@ -8,7 +8,6 @@
 <body>
   <h2>What is a Git Repo</h2>
   <p>Git is software for tracking changes in any set of files, usually used for coordinating work among programmers collaboratively developing source code during software development.
-
   </p>
 </body>
 
