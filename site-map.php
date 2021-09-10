6180370ee33412774e8f9b731ec681d671d6273c
Shinu <shinu@2hatslogic.com>
2021-09-10 16:11:23 +0530

    Added site map

---
+++ site-map.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+
+<head>
+  <title>Site map</title>
+</head>
+
+<body>
+  <div class="container">
+    <div class="row justify-content-center">
+      Lorem ipsum dolor sit amet, mea facete quodsi cu, mel et audire aliquid accumsan, no tota primis comprehensam mel. In fugit indoctum vis, no sea dicat altera convenire. Et noster tincidunt constituam qui, lucilius pertinax salutandi mel no. Te has quod ignota postea, illum sonet scaevola no eos, vel ea nulla tation.
+    </div>
+  </div>
+</body>
+
+</html>
